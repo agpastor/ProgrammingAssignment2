316616cb058eb5688e67f195c4001172951a1427
Agpastor <andreapastor13@gmail.com>
2014-04-25 22:15:27 -0700

    matrix cache assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCachematrix creates a 'special object' or list that takes can 
+## cache the inverse of a given matrix and overwrite the old cached value when 
+## data is updated
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {    # sets matrix to a new matrix
+      x <<- y    
+      inv <<- NULL          # invalidates cache of inverse
+    }
+    get <- function() x     #returns current matrix 
+    setInverse <- function(inverse) {
+      inv <<- inverse #sets new matrix inverse
+    }
+    getInverse <- function() inv #returns new matrix inverse
+    list(set = set, get = get,
+         setinverse = setInverse,
+         getinverse = getInverse)
+  }
 
-}
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## cacheSolve calls on subsetted elements of the  list that was 
+## created by makeCacheMatrix to return a new inverse when needed 
+
+cacheSolve <- function(x, ...) {     # x arg is the list that is fed in by makeCacheMatrix
+    inv <- x$getinverse()            # assigns currently stored inverse to inv
+    if(!is.null(inv)) {              #if the currently stored inverse is still valid 
+      message("getting cached data") # a msg is printed
+      return(inv)                    # and the cached matrix is returned
+    }
+    data <- x$get()          # retrieves updated matrix and assigns to data 
+    inv <- solve(data, ...)  # finds the inverse of the new matrix and sets it to inv
+    x$setinverse(inv)        # caches the new inverse 
+    inv 
+  }
+        ## Returns a matrix that is the inverse of 'x'
+
